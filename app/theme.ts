41ac6308b0c2d208f9e880b53b28388ba4ee4006
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-01 20:32:59 +0200

    Set red as primary color scheme

--- app/theme.ts
+++ app/theme.ts
@@ -1,10 +1,16 @@
-import { extendTheme } from '@chakra-ui/react'
+import { baseTheme, extendTheme, withDefaultColorScheme } from '@chakra-ui/react'
 
-const theme = extendTheme({
-  fonts: {
-    heading: `'Montserrat', sans-serif`,
-    body: `'Montserrat', sans-serif`,
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: `'Montserrat', sans-serif`,
+      body: `'Montserrat', sans-serif`,
+    },
+    colors: {
+      primary: baseTheme.colors.red,
+    }
   },
-})
+  withDefaultColorScheme({ colorScheme: 'primary' })
+)
 
 export default theme
