7ff3f07363d6bcb2504705f2fa0227e8f3bb4139
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-02 11:38:16 +0200

    Remove unwanted logo
    Replace with plain text denoting app name

--- app/components/AppTitle.tsx
+++ app/components/AppTitle.tsx
@@ -1,13 +1,10 @@
-import { HStack, Img } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import { Link } from "@remix-run/react";
-import logo from "~/../public/images/logo.png";
 
 export function AppTitle () {
   return (
     <Link to="/">
-      <HStack align="center" spacing={2}>
-        <Img height={"48px"} objectFit="contain" src={logo} alt="Shutter" />
-      </HStack>
+      <Heading>Shutter</Heading>
     </Link>
   )
 }
