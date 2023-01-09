c21ce3f74d5a18aef5a3d658cf4e98ed33631790
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-09 20:25:59 +0200

    Add custom centered view for feed

--- app/components/CustomComponents.tsx
+++ app/components/CustomComponents.tsx
@@ -1,6 +1,24 @@
-import { Card, useColorMode, VStack } from "@chakra-ui/react";
+import type { CardProps, StackProps } from "@chakra-ui/react";
+import { Card, HStack, useColorMode, VStack } from "@chakra-ui/react";
 import { RouteCatchBoundary, RouteErrorBoundary } from "remix-chakra-reusables";
 import { AppLinks } from "~/lib/links";
+
+interface FeedCenteredViewProps extends StackProps {
+  children: React.ReactNode;
+}
+
+export function FeedCenteredView (props: FeedCenteredViewProps) {
+  const { children, ...restOfProps } = props
+  return (
+    <>
+      <HStack justify="center" align="stretch" {...restOfProps}>
+        <VStack align="stretch" w={{ base: "100%", md: "60%", lg: "40%" }}>
+          {children}
+        </VStack>
+      </HStack>
+    </>
+  )
+}
 
 export function CustomCatchBoundary () {
   return (
@@ -24,15 +42,18 @@ export function CustomErrorBoundary ({ error }: { error: Error }) {
   )
 }
 
-interface CustomCardProps {
+interface CustomCardProps extends CardProps {
   children: React.ReactNode;
 }
 export function CustomCard (props: CustomCardProps) {
-  const { children } = props;
+  const { children, ...restOfProps } = props;
   const { colorMode } = useColorMode();
 
   return (
-    <Card bgColor={colorMode === "light" ? "white" : undefined}>
+    <Card
+      bgColor={colorMode === "light" ? "white" : undefined}
+      {...restOfProps}
+    >
       {children}
     </Card>
   )
