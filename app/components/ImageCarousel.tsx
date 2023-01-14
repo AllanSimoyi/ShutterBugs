0ceac8fb24c45e04f0bd8d8f918815ce662d5341
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-14 19:37:28 +0200

    Create reusable carousel component for post images

---
+++ app/components/ImageCarousel.tsx
@@ -0,0 +1,48 @@
+import { IconButton } from '@chakra-ui/react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+import { ChevronLeft, ChevronRight } from "tabler-icons-react";
+
+interface Props {
+  imageUrls: {
+    src: string;
+  }[]
+}
+
+export function ImageCarousel (props: Props) {
+  const { imageUrls } = props;
+
+  return (
+    <Carousel
+      images={imageUrls}
+      canAutoPlay={false}
+      objectFit="cover"
+      hasThumbnails={false}
+      hasDotButtons="bottom"
+      hasIndexBoard={false}
+      hasSizeButton={false}
+      isLoop={true}
+      shouldMaximizeOnClick={true}
+      shouldMinimizeOnClick={true}
+      leftIcon={(
+        <IconButton
+          aria-label='Previous Image'
+          size="sm"
+          borderRadius={"50%"}
+          icon={<ChevronLeft />}
+          m={2}
+        />
+      )}
+      rightIcon={(
+        <IconButton
+          aria-label='Next Image'
+          size="sm"
+          borderRadius={"50%"}
+          icon={<ChevronRight />}
+          m={2}
+        />
+      )}
+      style={{ flexGrow: 1, maxHeight: "400px" }}
+    />
+  )
+}
