cbeebf4c6340b2f1af2f8c36a437fdf090af9cb1
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-14 19:37:15 +0200

    Add custom root boundary component

--- app/components/CustomComponents.tsx
+++ app/components/CustomComponents.tsx
@@ -3,6 +3,28 @@ import { Card, HStack, useColorMode, VStack } from "@chakra-ui/react";
 import { RouteCatchBoundary, RouteErrorBoundary } from "remix-chakra-reusables";
 import { AppLinks } from "~/lib/links";
 
+export function CustomRootBoundaryError ({ error }: { error: Error }) {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="shadow-md rounded-lg p-6 space-y-2">
+        <h1 className="text-xl font-bold">
+          Error 500 - Internal Server Error
+        </h1>
+        <p>
+          We encountered an unexpected error. We're already working on fixing it. <br />
+          {error.message && (
+            <div className="font-bold py-2">
+              Detail: "{error.message}" <br />
+            </div>
+          )}
+          Please try reloading the page. <br />
+          If the issue pesists, <a className="underline text-blue-500" href={AppLinks.CustomerCare}>contact Customer Care</a>.
+        </p>
+      </div>
+    </div>
+  )
+}
+
 interface FeedCenteredViewProps extends StackProps {
   children: React.ReactNode;
 }
@@ -12,7 +34,8 @@ export function FeedCenteredView (props: FeedCenteredViewProps) {
   return (
     <>
       <HStack justify="center" align="stretch" {...restOfProps}>
-        <VStack align="stretch" w={{ base: "100%", md: "60%", lg: "40%" }}>
+        <VStack align="stretch" w={{ base: "100%", md: "80%", lg: "60%" }}>
+          {/* <VStack align="stretch" w={{ base: "100%", md: "60%", lg: "40%" }}> */}
           {children}
         </VStack>
       </HStack>
