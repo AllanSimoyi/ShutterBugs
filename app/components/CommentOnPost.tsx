3007d9af3c4ac73f87b1d9896f454b106a471441
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-15 20:03:56 +0200

    Extend container props

--- app/components/CommentOnPost.tsx
+++ app/components/CommentOnPost.tsx
@@ -1,49 +1,48 @@
-import { Button, CardFooter, HStack, Input } from "@chakra-ui/react";
+import type { StackProps } from "@chakra-ui/react";
+import { Button, HStack, Input } from "@chakra-ui/react";
 import 'react-gallery-carousel/dist/index.css';
 import { FormActionIdentifier, FormActionName } from "~/lib/forms.validations";
 
-interface Props {
+interface Props extends StackProps {
   postId: string;
   isSubmitting: boolean;
   commentRef: React.RefObject<HTMLInputElement>;
 }
 
 export function CommentOnPost (props: Props) {
-  const { postId, isSubmitting, commentRef } = props;
+  const { postId, isSubmitting, commentRef, ...restOfProps } = props;
 
   return (
-    <CardFooter flexDirection="column" alignItems="stretch" py={2} px={0}>
-      <HStack align="stretch">
-        <input
-          type="hidden"
-          name={FormActionName}
-          value={FormActionIdentifier.Comment}
-        />
-        <input
-          type="hidden"
-          name="postId"
-          value={postId}
-        />
-        <Input
-          p={0}
-          ref={commentRef}
-          type="text"
-          name="content"
-          variant="unstyled"
-          fontSize="sm"
-          placeholder='Add a comment...'
-          isDisabled={isSubmitting}
-          isRequired
-        />
-        <Button
-          type="submit"
-          variant="ghost"
-          fontSize="sm"
-          isDisabled={isSubmitting}
-        >
-          {isSubmitting ? "Posting..." : "Post"}
-        </Button>
-      </HStack>
-    </CardFooter>
+    <HStack align="stretch" py={2} px={0} {...restOfProps}>
+      <input
+        type="hidden"
+        name={FormActionName}
+        value={FormActionIdentifier.Comment}
+      />
+      <input
+        type="hidden"
+        name="postId"
+        value={postId}
+      />
+      <Input
+        p={0}
+        ref={commentRef}
+        type="text"
+        name="content"
+        variant="unstyled"
+        fontSize="sm"
+        placeholder='Add a comment...'
+        isDisabled={isSubmitting}
+        isRequired
+      />
+      <Button
+        type="submit"
+        variant="ghost"
+        fontSize="sm"
+        isDisabled={isSubmitting}
+      >
+        {isSubmitting ? "Posting..." : "Post"}
+      </Button>
+    </HStack>
   )
 }
