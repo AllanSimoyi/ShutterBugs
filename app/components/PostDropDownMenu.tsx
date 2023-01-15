6c2c751c2abaea3666f83a081208b47d89c19982
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-15 20:09:10 +0200

    Add drop down for actions on post

---
+++ app/components/PostDropDownMenu.tsx
@@ -0,0 +1,26 @@
+import { IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { DotsVertical } from "tabler-icons-react";
+
+interface Props {}
+
+export function PostDropDownMenu (props: Props) {
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        size="sm"
+        aria-label="Post Options"
+        icon={<DotsVertical data-testid="menu" />}
+        variant="ghost"
+      />
+      <MenuList>
+        <MenuItem fontSize={"sm"} py={2}>
+          Post Item 1
+        </MenuItem>
+        <MenuItem fontSize={"sm"} py={2}>
+          Post Item 2
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
