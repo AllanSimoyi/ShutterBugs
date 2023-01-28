725d051feff8adc7c4dd7d8fd5ea8621e6719f4e
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-28 19:25:02 +0200

    Factor out file input style, lint

--- app/components/AddImage.tsx
+++ app/components/AddImage.tsx
@@ -1,19 +1,14 @@
-import { Button, VStack } from "@chakra-ui/react";
-import type { ChangeEvent } from "react";
+import type { ChangeEvent } from 'react';
+
+import { Button, VStack } from '@chakra-ui/react';
+
+import { FILE_INPUT_STYLE } from '~/lib/constants';
 
 interface Props {
-  handleChange: (event: ChangeEvent<HTMLInputElement>) => void
+  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const fileInputStyle: React.CSSProperties = {
-  position: "absolute",
-  visibility: "hidden",
-  opacity: 0,
-  top: 0,
-  left: 0
-}
-
-export function AddImage (props: Props) {
+export function AddImage(props: Props) {
   const { handleChange } = props;
 
   return (
@@ -25,19 +20,19 @@ export function AddImage (props: Props) {
         accept="image/*"
         capture="environment"
         onChange={handleChange}
-        style={fileInputStyle}
+        style={FILE_INPUT_STYLE}
       />
-      <label htmlFor="file" style={{ cursor: "pointer", display: "block" }}>
+      <label htmlFor="file" style={{ cursor: 'pointer', display: 'block' }}>
         <Button
           type="submit"
           w="100%"
           fontSize="lg"
-          variant='solid'
-          style={{ pointerEvents: "none" }}
+          variant="solid"
+          style={{ pointerEvents: 'none' }}
         >
           Add Image
         </Button>
       </label>
     </VStack>
-  )
+  );
 }
