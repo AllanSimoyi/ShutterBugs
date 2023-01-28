bfe089d33ef0560f99019f9b0edc47586c03a775
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-28 19:26:45 +0200

    Factor our file input style

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,9 @@
-export const PRODUCT_NAME = "ShutterBugs";
+export const PRODUCT_NAME = 'ShutterBugs';
+
+export const FILE_INPUT_STYLE: React.CSSProperties = {
+  position: 'absolute',
+  visibility: 'hidden',
+  opacity: 0,
+  top: 0,
+  left: 0,
+};
