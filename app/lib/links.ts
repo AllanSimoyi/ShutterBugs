f2f1d4af4363a9c09c5b218b7511493769a8d652
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-28 19:27:00 +0200

    Add profile and edit profile links, lint

--- app/lib/links.ts
+++ app/lib/links.ts
@@ -1,8 +1,10 @@
 export const AppLinks = {
-  Home: "/",
-  Login: "/login",
-  Join: "/join",
-  CustomerCare: "/customer-care",
-  NewPost: "/posts/new",
+  Home: '/',
+  Login: '/login',
+  Join: '/join',
+  CustomerCare: '/customer-care',
+  NewPost: '/posts/new',
   Post: (postId: string) => `/posts/${postId}`,
-}
+  Profile: '/profile',
+  EditProfile: '/profile/edit',
+};
