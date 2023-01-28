556839112760ad70c5d583fa5eb92cc391224987
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-28 19:23:45 +0200

    Add custom eslint and prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,44 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    '@remix-run/eslint-config/jest-testing-library',
+    'prettier',
   ],
-  env: {
-    "cypress/globals": true,
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+        tabWidth: 2,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          ['sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+        pathGroups: [],
+        pathGroupsExcludedImportTypes: [],
+      },
+    ],
   },
-  plugins: ["cypress"],
+  env: {
+    'cypress/globals': true,
+  },
+  plugins: ['cypress', 'prettier'],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
