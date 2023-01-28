2c2faa7490b4526fe41196c3e85ac2865ef7d28d
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-28 19:25:16 +0200

    Lint, add padding

--- app/components/DropDownMenu.tsx
+++ app/components/DropDownMenu.tsx
@@ -1,13 +1,20 @@
-import { IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { Form, Link } from "@remix-run/react";
-import { ChevronDown } from "tabler-icons-react";
-import { AppLinks } from "~/lib/links";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import { Form, Link } from '@remix-run/react';
+import { ChevronDown } from 'tabler-icons-react';
+
+import { AppLinks } from '~/lib/links';
 
 interface Props {
   loggedIn: boolean;
 }
 
-export function DropDownMenu (props: Props) {
+export function DropDownMenu(props: Props) {
   const { loggedIn } = props;
 
   return (
@@ -23,14 +30,14 @@ export function DropDownMenu (props: Props) {
       <MenuList>
         {loggedIn && (
           <>
-            <MenuItem as={Link} to={AppLinks.Home} fontSize={"sm"} py={2}>
+            <MenuItem as={Link} to={AppLinks.Profile} fontSize={'sm'} py={4}>
               Profile
             </MenuItem>
-            <MenuItem as={Link} to={AppLinks.NewPost} fontSize={"sm"} py={2}>
+            <MenuItem as={Link} to={AppLinks.NewPost} fontSize={'sm'} py={4}>
               Create
             </MenuItem>
             <Form action="/logout" method="post">
-              <MenuItem type="submit" fontSize="sm" py={2}>
+              <MenuItem type="submit" fontSize="sm" py={4}>
                 Log Out
               </MenuItem>
             </Form>
@@ -38,15 +45,15 @@ export function DropDownMenu (props: Props) {
         )}
         {!loggedIn && (
           <>
-            <MenuItem as={Link} to={AppLinks.Login} fontSize={"sm"} py={2}>
+            <MenuItem as={Link} to={AppLinks.Login} fontSize={'sm'} py={2}>
               Log In
             </MenuItem>
-            <MenuItem as={Link} to={AppLinks.Join} fontSize={"sm"} py={2}>
+            <MenuItem as={Link} to={AppLinks.Join} fontSize={'sm'} py={2}>
               Create Account
             </MenuItem>
           </>
         )}
       </MenuList>
     </Menu>
-  )
+  );
 }
