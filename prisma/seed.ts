99fb487c651c34c10e4341b79da3ea07ca37a324
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-02-03 13:20:05 +0200

    Lint

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,11 @@
 import { faker } from '@faker-js/faker';
-import { PrismaClient } from "@prisma/client";
-import { createPasswordHash } from "~/lib/auth.server";
+import { PrismaClient } from '@prisma/client';
+
+import { createPasswordHash } from '~/lib/auth.server';
 
 const prisma = new PrismaClient();
 
-async function seed () {
+async function seed() {
   await prisma.commentLike.deleteMany();
   await prisma.comment.deleteMany();
   await prisma.postImage.deleteMany();
@@ -14,104 +15,95 @@ async function seed () {
 
   const Allan = await prisma.user.create({
     data: {
-      email: "allansimoyi@gmail.com",
-      fullName: "Allan",
-      picId: "glasses_trubcp",
-      hashedPassword: await createPasswordHash("jarnbjorn@7891"),
+      email: 'allansimoyi@gmail.com',
+      fullName: 'Allan',
+      picId: 'glasses_trubcp',
+      hashedPassword: await createPasswordHash('jarnbjorn@7891'),
     },
   });
 
   const Kudzie = await prisma.user.create({
     data: {
-      email: "kudziesimoyi@gmail.com",
-      fullName: "Kudzie",
-      picId: "Kudzie_gzmuzx",
-      hashedPassword: await createPasswordHash("jarnbjorn@7891"),
+      email: 'kudziesimoyi@gmail.com',
+      fullName: 'Kudzie',
+      picId: 'Kudzie_gzmuzx',
+      hashedPassword: await createPasswordHash('jarnbjorn@7891'),
     },
   });
 
-  await [...Array(10).keys()]
-    .reduce(async (acc, _) => {
-      await acc;
-      await prisma.post.create({
-        data: {
-          description: faker.lorem.paragraph(),
-          userId: Allan.id,
-          images: {
-            create: [
-              { imageId: "Image_One" },
-              { imageId: "Two_c11yl7" },
-              { imageId: "Three_whxzvj" },
-              { imageId: "Four_u2avvl" },
-            ]
-          },
-          likes: {
-            create: [
-              { userId: Kudzie.id },
-            ]
-          },
-          comments: {
-            create: [
-              { userId: Kudzie.id, content: faker.lorem.sentence() },
-              { userId: Allan.id, content: faker.lorem.sentence() },
-              { userId: Kudzie.id, content: faker.lorem.sentence() },
-              { userId: Allan.id, content: faker.lorem.sentence() },
-            ]
-          }
-        }
-      });
-    }, Promise.resolve());
+  await [...Array(10).keys()].reduce(async (acc, _) => {
+    await acc;
+    await prisma.post.create({
+      data: {
+        description: faker.lorem.paragraph(),
+        userId: Allan.id,
+        images: {
+          create: [
+            { imageId: 'Image_One' },
+            { imageId: 'Two_c11yl7' },
+            { imageId: 'Three_whxzvj' },
+            { imageId: 'Four_u2avvl' },
+          ],
+        },
+        likes: {
+          create: [{ userId: Kudzie.id }],
+        },
+        comments: {
+          create: [
+            { userId: Kudzie.id, content: faker.lorem.sentence() },
+            { userId: Allan.id, content: faker.lorem.sentence() },
+            { userId: Kudzie.id, content: faker.lorem.sentence() },
+            { userId: Allan.id, content: faker.lorem.sentence() },
+          ],
+        },
+      },
+    });
+  }, Promise.resolve());
 
-  await [...Array(10).keys()]
-    .reduce(async (acc, _) => {
-      await acc;
-      await prisma.post.create({
-        data: {
-          description: faker.lorem.paragraph(2),
-          userId: Kudzie.id,
-          images: {
-            create: [
-              { imageId: "Image_One" },
-              { imageId: "Two_c11yl7" },
-              { imageId: "Three_whxzvj" },
-              { imageId: "Four_u2avvl" },
-            ]
-          },
-          likes: {
-            create: [
-              { userId: Allan.id },
-            ]
-          },
-          comments: {
-            create: [
-              { userId: Kudzie.id, content: faker.lorem.sentence() },
-              { userId: Allan.id, content: faker.lorem.sentence() },
-              { userId: Kudzie.id, content: faker.lorem.sentence() },
-              { userId: Allan.id, content: faker.lorem.sentence() },
-            ]
-          }
-        }
-      });
-    }, Promise.resolve());
+  await [...Array(10).keys()].reduce(async (acc, _) => {
+    await acc;
+    await prisma.post.create({
+      data: {
+        description: faker.lorem.paragraph(2),
+        userId: Kudzie.id,
+        images: {
+          create: [
+            { imageId: 'Image_One' },
+            { imageId: 'Two_c11yl7' },
+            { imageId: 'Three_whxzvj' },
+            { imageId: 'Four_u2avvl' },
+          ],
+        },
+        likes: {
+          create: [{ userId: Allan.id }],
+        },
+        comments: {
+          create: [
+            { userId: Kudzie.id, content: faker.lorem.sentence() },
+            { userId: Allan.id, content: faker.lorem.sentence() },
+            { userId: Kudzie.id, content: faker.lorem.sentence() },
+            { userId: Allan.id, content: faker.lorem.sentence() },
+          ],
+        },
+      },
+    });
+  }, Promise.resolve());
 
   const posts = await prisma.post.findMany({
     select: {
       comments: {
         select: {
           id: true,
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   const commentIds = posts
-    .map(post => post.comments)
-    .map(comments => comments.map(comment => comment.id))
+    .map((post) => post.comments)
+    .map((comments) => comments.map((comment) => comment.id))
     .reduce((acc, commentIds) => {
-      return [
-        ...acc,
-        ...commentIds
-      ]
+      return [...acc, ...commentIds];
     }, []);
 
   await commentIds.reduce(async (acc, commentId) => {
@@ -120,7 +112,7 @@ async function seed () {
       data: {
         commentId: commentId,
         userId: Kudzie.id,
-      }
+      },
     });
   }, Promise.resolve());
 
