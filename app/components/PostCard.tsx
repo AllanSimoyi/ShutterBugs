0779ec58956db4b2addcc7e1779616ef661f4cb8
Allan Simoyi <allansimoyi@gmail.com>
2023-09-01 15:02:42 +0200

    Rearrange image card

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -51,18 +51,32 @@ export function PostCard(props: Props) {
       onClick={onSelect}
       className={twMerge(
         'group flex h-[80vh] flex-col items-stretch rounded-md bg-gradient-to-r from-black bg-cover bg-bottom bg-no-repeat',
-        'transition-all duration-300 hover:scale-[101%] hover:ring-stone-600'
+        'transition-all duration-300'
       )}
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(225, 225, 225, 0), rgba(0, 0, 0, 0.9)), url('${postImage}')`,
       }}
     >
+      <div className="flex flex-col items-stretch p-4">
+        <div className="flex flex-row items-center gap-4 rounded-lg bg-white/20 p-2 text-white">
+          <img
+            src={ownerImage}
+            alt={owner.name}
+            className="h-12 w-12 rounded-full object-cover"
+          />
+          <div className="flex flex-col items-start">
+            <span className="text-base tracking-wider">{owner.name}</span>
+            <span className="text-xs font-normal">{createdAt}</span>
+          </div>
+        </div>
+      </div>
+      <div className="grow" />
       {description && (
-        <div className="flex flex-col items-start p-4 opacity-0 transition-all duration-300 group-hover:opacity-100">
+        <div className="flex flex-col items-start p-4 transition-all duration-300">
           <span
             className={twMerge(
-              'bg-white/40 p-2 text-start text-sm font-normal tracking-wider text-stone-800',
-              'flex flex-row items-start gap-2 rounded-xl'
+              'bg-white/10 p-2 text-start text-sm font-normal tracking-wider text-white/50',
+              'flex flex-row items-start gap-2 rounded-lg'
             )}
           >
             <InfoCircle size={20} />
@@ -70,18 +84,6 @@ export function PostCard(props: Props) {
           </span>
         </div>
       )}
-      <div className="grow" />
-      <div className="flex flex-row items-center gap-4 p-4 text-white">
-        <img
-          src={ownerImage}
-          alt={owner.name}
-          className="h-12 w-12 rounded-full object-cover"
-        />
-        <div className="flex flex-col items-start">
-          <span className="text-base tracking-wider">{owner.name}</span>
-          <span className="text-xs font-light">{createdAt}</span>
-        </div>
-      </div>
     </button>
   );
 }
